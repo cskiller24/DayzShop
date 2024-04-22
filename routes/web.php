cf131634582a46d2fa2bccb77e6bcbe537fc4885
cskiller24 <ccadayong2020@plm.edu.ph>
2024-04-22 23:33:56 +0800

    wip: base landing page

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use App\Livewire\Auth\ForgotPassword;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
 use App\Livewire\Auth\ResetPassword;
+use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Welcome::class)
+    ->name('welcome');
 
 Route::get('login', Login::class)
     ->middleware('guest')
