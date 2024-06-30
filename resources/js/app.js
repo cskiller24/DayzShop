b094ecddbdf917f076df122539d91797a06d0aa3
cskiller24 <ccadayong2020@plm.edu.ph>
2024-07-01 00:36:07 +0800

    feat: masking and remove jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,15 @@
 import * as bootstrap from "bootstrap";
 import "@tabler/core"
-import $ from 'jquery';
-import {Livewire} from '../../vendor/livewire/livewire/dist/livewire.esm';
+import {Alpine, Livewire} from '../../vendor/livewire/livewire/dist/livewire.esm';
 import axios from "axios";
+import {mask} from "@alpinejs/mask";
 
 import.meta.glob(["../images/**", "../fonts/**"]);
 
 window.bootstrap = bootstrap;
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.$ = $;
+Alpine.plugin(mask);
 Livewire.start();
 
 // import "./toggle";
