6cd98d2edf2cc7cc300a266da5dc9d9990726e89
Cedy Cadayong <78188741+cskiller24@users.noreply.github.com>
2025-02-28 10:24:21 +0800

    chore: remove insights (#101)
    * chore: remove insights
    
    * fix: tests

--- app/Livewire/Seller/Components/Products/Table.php
+++ app/Livewire/Seller/Components/Products/Table.php
@@ -6,19 +6,25 @@ namespace App\Livewire\Seller\Components\Products;
 
 use App\Livewire\Components\Toaster;
 use App\Models\Product;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Throwable;
 
 class Table extends Component
 {
     use WithPagination;
 
+    /**
+     * @throws AuthorizationException
+     * @throws Throwable
+     */
     public function delete(string $id): void
     {
         $this->authorize('delete', Product::class);
-        
+
         DB::transaction(function () use ($id): void {
             $product = Product::query()->findOrFail($id);
             $product->categories()->sync([], true);
